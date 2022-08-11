65ffa6dc0db2bad472eab0fbcf5aabd682079966
xiaoke <yjmqaz@126.com>
2022-08-11 22:14:19 +0800

    22/8/11

---
+++ src/main/java/com/yeffxyz/blog/mapper/RoleMenuMapper.java
@@ -0,0 +1,18 @@
+package com.yeffxyz.blog.mapper;
+
+import com.yeffxyz.blog.entity.RoleMenu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 角色菜单持久层接口
+ *
+ * @author xoke
+ * @date 2022/8/11
+ */
+public interface RoleMenuMapper extends BaseMapper<RoleMenu> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yeffxyz/blog/mapper/RoleResourceMapper.java
@@ -0,0 +1,18 @@
+package com.yeffxyz.blog.mapper;
+
+import com.yeffxyz.blog.entity.RoleResource;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 角色资源持久层接口
+ *
+ * @author xoke
+ * @date 2022/8/11
+ */
+public interface RoleResourceMapper extends BaseMapper<RoleResource> {
+
+}
+
+
+
+
